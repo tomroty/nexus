15739f51fbd358456f4e1dfddbe13a4d44f1cebc
tomroty <tom.roty@proton.me>
2025-03-01 20:36:38 +0100

    implement post creation and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import CreatePost from "@/components/CreatePost";
+import { currentUser } from "@clerk/nextjs/server";
 import React from "react";
 
 export default async function Home() {
+  const user = await currentUser();
   return (
-
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
+      </div>
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        WhoToFollow
+      </div>
+    </div>
   );
 }
