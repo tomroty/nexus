fdc820900fbecdffc842a0a2ce4bdcd2cdbb6c5c
tomroty <tom.roty@proton.me>
2025-03-01 23:19:57 +0100

    add "Who To Follow" functionnality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import CreatePost from "@/components/CreatePost";
+import WhoToFollow from "@/components/WhoToFollow";
 import { currentUser } from "@clerk/nextjs/server";
 import React from "react";
 
@@ -8,9 +9,10 @@ export default async function Home() {
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">
         {user ? <CreatePost /> : null}
+        
       </div>
       <div className="hidden lg:block lg:col-span-4 sticky top-20">
-        WhoToFollow
+        <WhoToFollow />
       </div>
     </div>
   );
