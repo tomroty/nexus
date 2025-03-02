a9dc64f41e49bb59f739f5437b8ac877972d45c5
tomroty <tom.roty@proton.me>
2025-03-02 23:00:11 +0100

    finalize image upload

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -1,18 +1,30 @@
 "use client";
 
 import { UploadDropzone } from "@/lib/uploadthing";
+import { XIcon } from "lucide-react";
 import React from "react";
 
 interface ImageUploadProps {
   onChange: (url: string) => void;
   value: string;
-  endpoint: "postImage";
+  endpoint: "imageUploader";
 }
 
 function ImageUpload({ endpoint, onChange, value }: ImageUploadProps) {
-
-  
-
+  if (value) {
+    return (
+      <div className="relative size-40">
+        <img src={value} alt="Upload" className="rounded-md size-40 object-cover" />
+        <button
+          onClick={() => onChange("")}
+          className="absolute top-0 right-0 p-1 bg-red-500 rounded-full shadow-sm"
+          type="button"
+        >
+          <XIcon className="h-4 w-4 text-white" />
+        </button>
+      </div>
+    );
+  }
   return (
     <UploadDropzone
       endpoint={endpoint}
