c2468dc3fd3fb2ba3ca3711d205436ff990a67a6
Tom Roty <tom.roty@proton.me>
2025-03-04 08:58:49 +0100

    remove dev mode clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,9 +29,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        layout: {
+          unsafe_disableDevelopmentModeWarnings: true,
+        },
+      }}
+    >
       <html lang="en" suppressHydrationWarning>
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
